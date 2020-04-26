a35bae2d4f78f458daf28d02b5f963608a87dbfc
Igor Makarov <igormaka@gmail.com>
2020-04-26 18:20:54 +0300

    redirect root (for now)

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -2,4 +2,5 @@
 
 mkdir -p _site || true
 
+cp netlify_redirects.txt _site/_redirects
 curl --compress https://www.parsehub.com/api/v2/projects/$PARSEHUB_PROJECT_KEY/last_ready_run/data?api_key=$PARSEHUB_API_KEY > _site/data.json
