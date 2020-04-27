20d4420b5f2bbd020013fd22012fefdc9f01d673
Igor Makarov <igormaka@gmail.com>
2020-04-28 00:37:57 +0300

    turns out google analytics wasn't working

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -6,6 +6,7 @@ mkdir -p _data || true
 cp netlify_redirects.txt _site/_redirects
 curl --compress https://www.parsehub.com/api/v2/projects/$PARSEHUB_PROJECT_KEY/last_ready_run/data?api_key=$PARSEHUB_API_KEY > _data/data.json
 
+export JEKYLL_ENV=production
 bundle exec jekyll build
 
 cp _data/data.json _site/data.json
