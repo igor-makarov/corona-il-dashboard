c7d4a4800d6ed84cf758dc73423675b7c79e97d4
Igor Makarov <igormaka@gmail.com>
2020-05-02 10:16:03 +0300

    add parsehub proxy

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -4,10 +4,12 @@ mkdir -p _site || true
 mkdir -p _data || true
 
 cp netlify_redirects.txt _site/_redirects
-curl --compress https://www.parsehub.com/api/v2/projects/$PARSEHUB_PROJECT_KEY/last_ready_run/data?api_key=$PARSEHUB_API_KEY > _data/data.json
+PARSEHUB_DATA_URL="https://www.parsehub.com/api/v2/projects/$PARSEHUB_PROJECT_KEY/last_ready_run/data?api_key=$PARSEHUB_API_KEY"
+curl --compress "$PARSEHUB_DATA_URL" > _data/data.json
 bundle exec ruby process_data.rb
 
 export JEKYLL_ENV=production
 bundle exec jekyll build
 
+echo "/raw_data.json $PARSEHUB_DATA_URL 200" >> _site/_redirects
 cp _data/data.json _site/data.json
