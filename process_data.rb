5d420612d231212ae94fcd803f9a12152dfed8cb
Igor Makarov <igormaka@gmail.com>
2020-05-08 14:51:35 +0300

    print timestamp

--- process_data.rb
+++ process_data.rb
@@ -3,6 +3,7 @@ require 'json'
 json_str = File.read('_data/data.json')
 json = JSON.parse(json_str)
 
+STDERR.puts "Timestamp: #{json['timestamp']}"
 fatalities = json['deceased'].delete(',').to_i
 total_outcome = fatalities + json['recovered'].delete(',').to_i
 STDERR.puts "Fatalities: #{fatalities}"
